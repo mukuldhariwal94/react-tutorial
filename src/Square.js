4e7cf0eed14785724ab476171770a3b2168f19bb
Dhariwal,Mukul <mukul.dhariwal94@gmail.com>
2019-04-19 22:59:49 +0530

    structuring code

---
+++ src/Square.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+export default Square; 
